8ebefc61997669965fb0972429f8963ec585a648
Brian Takita <brian.takita@gmail.com>
2022-09-01 11:33:36 -0500

    update dependencies

--- src/mean_.js
+++ src/mean_.js
@@ -1,8 +1,6 @@
 import { sum } from './add/index.js'
 /**
  * Average of the numerator_a
- * @type {import('./mean_.d.ts').mean_}
- * @param {number[]} numerator_a
  */
 export const mean_ = numerator_a=>
 	sum(numerator_a) / numerator_a.length
