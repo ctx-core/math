4764cb7bb548031f8ecfd7394069d5fef3154be8
Brian Takita <brian.takita@gmail.com>
2022-09-05 03:36:10 -0500

    add: test NaN values

--- src/add/index.test.ts
+++ src/add/index.test.ts
@@ -4,6 +4,7 @@ import { add } from '../index.js'
 test('add|value', ()=>{
 	equal(add([2, 1, 3, 0]), 6)
 	equal(add([2, 1, 3, null]), 6)
+	equal(add([NaN, NaN]), null)
 	equal(add([null, null, null, null]), null)
 	equal(add([undefined, undefined]), null)
 	equal(add(null), null)
